6b5c42d1f82d63a36b912f2d8940e7fccd8f05ff
TimIsabella <timjisabella@gmail.com>
2022-08-06 15:14:44 -0700

    Separated objects

---
+++ Mosh/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Mosh
+{
+	public class Person
+	{
+		//public string FirstName;
+		//public string LastName;
+
+		public void Introduce(string firstName = "Not", string lastName = "Sure")
+		{
+			//Console.WriteLine("My name is " + FirstName + " " + LastName);
+			Console.WriteLine("My name is " + firstName + " " + lastName);
+		}
+	}
+}
--- Mosh/Program.cs
+++ Mosh/Program.cs
@@ -10,21 +10,8 @@ namespace Mosh
 			//john.FirstName = "John";
 			//john.LastName = "Smith";
 			john.Introduce("John", "Smith");
-			//john.Introduce();
 
 			ArraysExamples.ArraysMain();
-		}
-	}
-
-	public class Person
-	{
-		//public string FirstName;
-		//public string LastName;
-
-		public void Introduce(string firstName = "Not", string lastName = "Sure")
-		{
-			//Console.WriteLine("My name is " + FirstName + " " + LastName);
-			Console.WriteLine("My name is " + firstName + " " + lastName);
 		}
 	}
 }
