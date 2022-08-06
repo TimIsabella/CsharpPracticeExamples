ad248aae9da41f76aa26fe48c425529c366876fa
TimIsabella <timjisabella@gmail.com>
2022-08-06 14:39:15 -0700

    Arrays

---
+++ Mosh/ArraysExamples.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mosh
+{
+	public class ArraysExamples
+	{
+		public static void ArraysMain()
+		{
+			//Array
+			int[] arr1 = new int[] { 1, 2, 3, };
+			Console.WriteLine("arr1[2]: " + arr1[2]);
+
+			//Array
+			var arr2 = new[] { 11, 22, 33 };
+			Console.WriteLine("arr2[1]: " + arr1[1]);
+			
+			//Array length
+			Console.WriteLine("arr2 length: " + arr1.Length + " elements");
+
+			//Array IndexOf()
+			Console.WriteLine("arr2 IndexOf() '3': " + Array.IndexOf(arr1, 3));
+
+			//Matrix (rectangular)
+			var mat1 = new bool[,] { 
+									{ true, false, false },
+									{ true, true, false },
+									{ true, false, true }
+								  };
+			Console.WriteLine("mat1[1, 2]: " + mat1[1, 2]);
+
+			//Matrix (jagged 'array of arrays')
+			var mat2 = new char[][] {
+									new char[] { 'a' },
+									new char[] { 'b', 'c', 'd', 'e', 'f', 'g' },
+									new char[] { 'i', 'h' }
+								   };
+			Console.WriteLine("mat2[1][5]: " + mat2[1][5]);
+
+			//Mixed types with 'object'
+
+			//Matrix (rectangular) of different types
+			var mat3 = new object[,] {
+										{ true, false, true },
+										{ "this", "that", "other" },
+										{ 7, 8, 9 }
+									 };
+			Console.WriteLine("mat3[1, 2]: " + mat3[1, 2]);
+
+			//Matrix (jagged) of different types
+			var mat4 = new object[][] {
+										new object[] { "this", "that" },
+										new object[] { false, true, false},
+										new object[] { 1, 2, 3, 4, 5}
+									  };
+			Console.WriteLine("mat4[2][4]: " + mat4[2][4]);
+
+			///////////Objects///////////////////////////////////////////////////////////////////////////////////////////////////
+
+			//Array Object
+			Arrays arrObj2 = new Arrays(new int[] { 1, 2, 3 }, new double[] { 4.4, 5.5, 6.6 }, new string[] { "This", "That", "Other" });
+			Console.WriteLine("arrObj2.Array1[0]: " + arrObj2.Array1[1]);
+			Console.WriteLine("arrObj2.Array1[1]: " + arrObj2.Array1[2]);
+			Console.WriteLine("arrObj2.Array1[2]: " + arrObj2.Array1[2]);
+
+			//Array Object
+			Arrays arrObj3 = new Arrays(new int[0], new double[0], new string[0]);
+			arrObj3.Array1 = new int[] { 11, 22, 33 };
+			Console.WriteLine("arrObj3.Array1[0]: " + arrObj3.Array1[0]);
+
+			//Matrix object (rectangular)
+			MatrixRectangular matrixSquareObj = new MatrixRectangular(new int[0, 0]);
+			matrixSquareObj.Matrix1 = new int[,] { 
+													{ 1, 2, 3 },
+													{ 4, 5, 6 },
+													{ 7, 8, 9 } 
+												 };
+			Console.WriteLine("matrixSquareObj.Matrix1[2,1]: " + matrixSquareObj.Matrix1[2,1]);
+
+			//Matrix object (jagged 'array of arrays')
+			MatrixJagged matrixJaggedObj = new MatrixJagged(new int[0][]);
+			matrixJaggedObj.Matrix1 = new int[][] { 
+													new[] { 1, 2 },
+													new[] { 3 },
+													new[] { 4, 5, 6, 7 },
+													new[] { 8, 9 } 
+												  };
+			Console.WriteLine("matrixJaggedObj.Matrix1[1][1]: " + matrixJaggedObj.Matrix1[2][3]);
+		}
+
+		public class Arrays
+		{
+			//Constructor
+			public Arrays(int[] a1, double[] a2, string[] a3)
+			{
+				array1 = a1;
+				array2 = a2;
+				array3 = a3;
+			}
+
+			//Fields	
+			private int[] array1;
+			private double[] array2 = new double[3];  //Allocating memory of 3 elements
+			private string[] array3 = new string[3];
+
+			//Properties
+			public int[] Array1
+			{
+				get { return array1; }
+				set { array1 = value; }
+			}
+
+			//Properties
+			public double[] Array2
+			{
+				get { return array2; }
+				set { array2 = value; }
+			}
+
+			//Properties
+			public string[] Array3
+			{
+				get { return array3; }
+				set { array3 = value; }
+			}
+		}
+
+		public class MatrixRectangular
+		{
+			public MatrixRectangular(int[,] m1)
+			{
+				matrix1 = m1;
+			}
+
+			private int[,] matrix1 = new int[0, 0];
+
+			public int[,] Matrix1
+			{
+				get { return matrix1; }
+				set { matrix1 = value; }
+			}
+		}
+
+		public class MatrixJagged
+		{
+			public MatrixJagged(int[][] m1)
+			{
+				matrix1 = m1;
+			}
+
+			private int[][] matrix1 = new int[0][];
+
+			public int[][] Matrix1
+			{
+				get { return matrix1; }
+				set { matrix1 = value; }
+			}
+		}
+	}
+}
--- Mosh/Program.cs
+++ Mosh/Program.cs
@@ -12,23 +12,7 @@ namespace Mosh
 			john.Introduce("John", "Smith");
 			//john.Introduce();
 
-			//Arrays
-			Arrays arr = new Arrays(new int[] { 1, 2, 3 }, new double[] { 4.4, 5.5, 6.6 }, new string[] { "This", "That", "Other" });
-			Console.WriteLine(arr.Array1[0] + " " + arr.Array2[1] + " " + arr.Array3[2]);
-
-			Arrays arr2 = new Arrays(new int[0], new double[0], new string[0]);
-			arr2.Array1 = new int[] { 11, 22, 33 };
-			Console.WriteLine("arr.Array1[0]: " + arr2.Array1[0]);
-
-			//Matrix (Rectangular)
-			MatrixRectangular matrixSquare = new MatrixRectangular(new int[0, 0]);
-			matrixSquare.Matrix1 = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-			Console.WriteLine("matrixSquare.Matrix1[2,1]: " + matrixSquare.Matrix1[2,1]);
-
-			//Matrix (Jagged)
-			MatrixJagged matrixJagged = new MatrixJagged(new int[0][]);
-			matrixJagged.Matrix1 = new int[][] { new[] { 1, 2, 3 }, new[] { 4, 5, 6 } };
-			Console.WriteLine("matrixJagged.Matrix1[1][1]: " + matrixJagged.Matrix1[1][1]);
+			ArraysExamples.ArraysMain();
 		}
 	}
 
@@ -41,75 +25,6 @@ namespace Mosh
 		{
 			//Console.WriteLine("My name is " + FirstName + " " + LastName);
 			Console.WriteLine("My name is " + firstName + " " + lastName);
-		}
-	}
-
-	public class Arrays
-	{
-		//Constructor
-		public Arrays(int[] a1, double[] a2, string[] a3)
-		{
-			array1 = a1;
-			array2 = a2;
-			array3 = a3;
-		}
-
-		//Fields	
-		private int[] array1;
-		private double[] array2 = new double[3];  //Allocating memory of 3 elements
-		private string[] array3 = new string[3];
-
-		//Properties
-		public int[] Array1
-		{
-			get { return array1; }
-			set { array1 = value; }
-		}
-
-		//Properties
-		public double[] Array2
-		{
-			get { return array2; }
-			set { array2 = value; }
-		}
-
-		//Properties
-		public string[] Array3
-		{
-			get { return array3; }
-			set { array3 = value; }
-		}
-	}
-
-	public class MatrixRectangular
-	{
-		public MatrixRectangular(int[,] m1)
-		{
-			matrix1 = m1;
-		}
-		
-		private int[,] matrix1 = new int[0,0];
-
-		public int[,] Matrix1
-		{
-			get { return matrix1; }
-			set { matrix1 = value; }
-		}
-	}
-
-	public class MatrixJagged
-	{
-		public MatrixJagged(int[][] m1)
-		{
-			matrix1 = m1;
-		}
-
-		private int[][] matrix1 = new int[0][];
-
-		public int[][] Matrix1
-		{
-			get { return matrix1; }
-			set { matrix1 = value; }
 		}
 	}
 }
