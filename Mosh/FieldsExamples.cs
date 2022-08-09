d5af01eb2bfda1a485f51c46ddd9665ea7df4840
TimIsabella <timjisabella@gmail.com>
2022-08-09 09:44:28 -0700

    Firlds

---
+++ Mosh/FieldsExamples.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mosh
+{
+	public class FieldsExamples
+	{
+		public int Id;
+		public string Name;
+
+		public List<OrderObject> NotInitializedOrders; //NOT initialized
+		public List<OrderObject> InitializedOrders = new List<OrderObject>(); //Initialized
+		readonly List<OrderObject> ReadOnlyOrders = new List<OrderObject>(); //List is 'readonly'
+
+		public FieldsExamples(int id)
+		{
+			this.Id = id;
+		}
+
+		public FieldsExamples(int id, string name) : this(id)
+		{
+			//this.Id = id;		//'this(id)' above serves the same purpose by inheritance
+			this.Name = name;
+		}
+
+		public void FieldsExamplesMain()
+		{
+			//Initialize an empty list of 'OrderObject'
+			NotInitializedOrders = new List<OrderObject>();
+		}
+	}
+}
