673fba1bb53cf379277ab85bfa3ba8789fa5efde
TimIsabella <timjisabella@gmail.com>
2022-08-09 18:15:02 -0700

    ...

--- Mosh/PersonObject.cs
+++ Mosh/PersonObject.cs
@@ -11,7 +11,7 @@ namespace Mosh
 		public void Introduce(string firstName = "Not", string lastName = "Sure")
 		{
 			//Console.WriteLine("My name is " + FirstName + " " + LastName);
-			Console.WriteLine("My name is {0} {1}", firstName, lastName);
+			Console.WriteLine("My name is '{0} {1}'", firstName, lastName);
 		}
 
 		public static PersonObject Parse(string str)
