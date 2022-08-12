d9bca4ee4b9526035bde8301f83fcb2440bcc089
TimIsabella <timjisabella@gmail.com>
2022-08-12 12:35:09 -0700

    ...

--- Mosh/MethodOverridingExamples.cs
+++ Mosh/MethodOverridingExamples.cs
@@ -19,10 +19,10 @@ namespace Mosh
 			//var triangle = new Shapes.Circle(); triangle.Draw();
 			//var image = new Image(); image.Draw();
 
-			var shapes = new List<Shape>(); //List of 'Shape' as objects below all inherit from 'Shape'
-			shapes.Add(new Circle());
-			shapes.Add(new Square());
-			shapes.Add(new Triangle());
+			var shapes = new List<ShapeObjects.Shape>(); //List of 'Shape' as objects below all inherit from 'Shape'
+			shapes.Add(new ShapeObjects.Circle());
+			shapes.Add(new ShapeObjects.Square());
+			shapes.Add(new ShapeObjects.Triangle());
 
 			foreach(var shape in shapes)
 			{
@@ -31,42 +31,47 @@ namespace Mosh
 		}
 	}
 
-	//Base shape object
-	public class Shape
+	//All Shapes
+	public class ShapeObjects
 	{
-		public int Width;
-		public int Height;
-		public virtual void Draw()  //'virtual' designation allows derived objects to replace this default method when inherited
-		{ Console.WriteLine("Shape: This is the default 'Draw' method."); }
-	}
+		//Base shape object
+		public class Shape
+		{
+			public int Width;
+			public int Height;
+			public virtual void Draw()  //'virtual' designation allows derived objects to replace this default method when inherited
+			{ Console.WriteLine("Shape: This is the default 'Draw' method."); }
+		}
 
-	public class Circle : Shape
-	{
-		public int Radius;
-		public override void Draw()  //Replacement of inherited method by using 'override'
-		{ Console.WriteLine("Circle: This 'Draw' method has been overridden."); }
-	}
+		public class Circle : Shape
+		{
+			public int Radius;
+			public override void Draw()  //Replacement of inherited method by using 'override'
+			{ Console.WriteLine("Circle: This 'Draw' method has been overridden."); }
+		}
 
-	public class Square : Shape
-	{
-		public int Diameter;
-		public override void Draw()  //Replacement of inherited method by using 'override'
-		{ Console.WriteLine("Square: This 'Draw' method has been overridden."); }
-	}
+		public class Square : Shape
+		{
+			public int Diameter;
+			public override void Draw()  //Replacement of inherited method by using 'override'
+			{ Console.WriteLine("Square: This 'Draw' method has been overridden."); }
+		}
 
-	public class Triangle : Shape
-	{
-		public int Tangent;
-		public override void Draw()  //Replacement of inherited method by using 'override'
-		{ Console.WriteLine("Triangle: This 'Draw' method has been overridden."); }
-	}
+		public class Triangle : Shape
+		{
+			public int Tangent;
+			public override void Draw()  //Replacement of inherited method by using 'override'
+			{ Console.WriteLine("Triangle: This 'Draw' method has been overridden."); }
+		}
 
-	public class Image : Shape
-	{
-		public int Resolution;
+		//No method override
+		public class Image : Shape
+		{
+			public int Resolution;
 
-		//No method override, so default method inherited from 'Shape' will be used
-		//public override void Draw()
-		//{ Console.WriteLine("Image: This 'Draw' method has been overridden"); }
+			//No method override, so default method inherited from 'Shape' will be used
+			//public override void Draw()
+			//{ Console.WriteLine("Image: This 'Draw' method has been overridden"); }
+		}
 	}
 }
