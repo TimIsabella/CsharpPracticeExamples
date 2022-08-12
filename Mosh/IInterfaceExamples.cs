af2ded8b15cb71cb1a383174fb6c1501a678573f
TimIsabella <timjisabella@gmail.com>
2022-08-12 14:02:53 -0700

    Interfaces

---
+++ Mosh/IInterfaceExamples.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mosh
+{
+	//Interface is declared as 'interface' and no access modifier
+	interface IInterfaceExamples
+	{
+		
+	}
+}
