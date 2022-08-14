5ce521f16d6958f22abb5923b225fb97fd55f3d5
TimIsabella <timjisabella@gmail.com>
2022-08-13 17:11:32 -0700

    Interface

---
+++ Mosh/InterfaceExamplesAdvanced.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mosh
+{
+	public class InterfaceExamplesAdvanced
+	{
+		public static void InterfaceExamplesAdvancedMain()
+		{
+			Console.WriteLine("\n *********** INTERFACE EXAMPLES ADVANCED *********** \n");
+
+			var orderProcessor = new OrderProcessor();
+			var order = new Order { DatePlaced = DateTime.Now, TotalPrice = 100f };
+			orderProcessor.Process(order);
+		}
+
+		//Interface is declared as 'interface' and no access modifier
+		interface IInterfaceExamples
+		{
+
+		}
+
+		///////////////////////////////// Unit Test Code for Interface Below /////////////////////////////////
+
+		public class OrderProcessor
+		{
+			private readonly ShippingCalculator _shippingCalculator;
+
+			public OrderProcessor()
+			{ _shippingCalculator = new ShippingCalculator(); }
+
+			public void Process(Order order)
+			{
+				if(order.IsShipped) throw new InvalidOperationException("This order is already processed.");
+				order.Shipment = new Shipment { Cost = _shippingCalculator.CalculateShipping(order), ShippingDate = DateTime.Today.AddDays(1) };
+			}
+		}
+
+		public class ShippingCalculator
+		{
+			public float CalculateShipping(Order order)
+			{ 
+				if(order.TotalPrice < 30f) return order.TotalPrice * 0.1f; 
+				else return 0; 
+			}
+		}
+
+		public class Shipment
+		{
+			public float Cost { get; set; }
+			public DateTime ShippingDate { get; set; }
+		}
+
+		public class Order
+		{
+			public int Id { get; set; }
+			public DateTime DatePlaced { get; set; }
+			public Shipment Shipment { get; set; }
+			public float TotalPrice { get; set; }
+			
+			public bool IsShipped
+			{
+				get { return Shipment != null; }
+			}
+		}
+	}
+}
