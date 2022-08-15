418e58202fed66b83f9b69b6b65eccd85cfb5641
TimIsabella <timjisabella@gmail.com>
2022-08-15 11:16:20 -0700

    Polymorphism

---
+++ Mosh/InterfacePolymorphismExamples.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mosh
+{
+	public class InterfacePolymorphismExamples
+	{
+		public static void InterfacePolymorphismExamplesMain()
+		{
+			var encoder = new VideoEncoder();
+			encoder.Encode(new Video());
+		}
+
+		public class VideoEncoder
+		{
+			private readonly MailService _mailService;
+
+			public VideoEncoder()
+			{
+				_mailService = new MailService();
+			}
+
+			public void Encode(Video video)
+			{
+				// Video encoding code
+
+				_mailService.Send(new Mail());
+			}
+		}
+
+		public class MailService
+		{
+			public void Send(Mail mail)
+			{
+				Console.WriteLine("Sending mail...");
+			}
+		}
+
+		public class Video
+		{}
+
+		public class Mail
+		{}
+	}
+}
