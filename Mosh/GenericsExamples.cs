ea7c5e847b1adff5692f629b46c5e6b4ee260041
TimIsabella <timjisabella@gmail.com>
2022-08-16 13:02:20 -0700

    Generics

--- Mosh/GenericsExamples.cs
+++ Mosh/GenericsExamples.cs
@@ -9,26 +9,48 @@ namespace Mosh
 		{
 			Console.WriteLine("\n *********** GENERICS *********** \n");
 
-			var book = new Book(new string[] { "111", "This book" });
-			Console.WriteLine($"Book ISBN: {book.Isbn}, Book Title: {book.Title}");
-
+			//Normal list -- instantiated to take integers
 			var numList = new List<int>();
 			numList.Add(11); numList.Add(22); numList.Add(33);
-			Console.WriteLine($"numList [0]={numList[0]}, [1]={numList[1]}, [2]={numList[2]}");
+			Console.WriteLine($"numList -- [0]={numList[0]}, [1]={numList[1]}, [2]={numList[2]}");
 
-			var numGenericList = new GenericList<int>();
-			numGenericList.Add(33); numGenericList.Add(66); numGenericList.Add(99);
-			Console.WriteLine($"numGenericList [0]={numGenericList[0]}, [1]={numGenericList[1]}, [2]={numGenericList[2]}");
+			///////////
 
-			var booksGenericsList = new GenericList<Book>();
-			booksGenericsList.Add(new Book(new string[] { "222", "Generic book" }));
-			Console.WriteLine($"booksGenericsList ISBN: {booksGenericsList[0].Isbn}, Book Title: {booksGenericsList[0].Title}");
+			//Generic list class -- instantiated to take bools
+			var boolGenericList = new GenericClassList<bool>();
+			boolGenericList.AddToList(true); boolGenericList.AddToList(false); boolGenericList.AddToList(true);
+			Console.WriteLine($"boolGenericList -- [0]={boolGenericList[0]}, [1]={boolGenericList[1]}, [2]={boolGenericList[2]}");
 
-			var genericBookDictionary = new GenericsDictionary<string, Book>();
+			//Generic list class -- instantiated to take ints
+			var numGenericList = new GenericClassList<int>();
+			numGenericList.AddToList(33); numGenericList.AddToList(66); numGenericList.AddToList(99);
+			Console.WriteLine($"numGenericList -- [0]={numGenericList[0]}, [1]={numGenericList[1]}, [2]={numGenericList[2]}");
+
+			//Generic list class -- instantiated to take strings
+			var stringGenericList = new GenericClassList<string>();
+			stringGenericList.AddToList("This"); stringGenericList.AddToList("That"); stringGenericList.AddToList("Those");
+			Console.WriteLine($"stringGenericList -- [0]={stringGenericList[0]}, [1]={stringGenericList[1]}, [2]={stringGenericList[2]}");
+
+			///////////
+
+			//Book class -- takes two string inputs
+			var book = new Book(new string[] { "111", "This book" });
+			Console.WriteLine($"Book -- ISBN: {book.Isbn}, Book Title: {book.Title}");
+
+			//Generic list class -- instantiated to take Book classes
+			var booksGenericsList = new GenericClassList<Book>();
+			booksGenericsList.AddToList(new Book(new string[] { "222", "Generic book" }));
+			Console.WriteLine($"booksGenericsList -- ISBN: {booksGenericsList[0].Isbn}, Book Title: {booksGenericsList[0].Title}");
+
+			//Generic dictionary class -- instantiated to take Book classes with key value as string
+			//- Dictionaries are indexed by key value
+			var genericBookDictionary = new GenericDictionary<string, Book>();
 			genericBookDictionary.AddToDictionary("11", new Book(new string[] { "333", "Dictionary book" }));
-			Console.WriteLine($"Entry '11' book title is '{genericBookDictionary["11"].Title}'");
+			Console.WriteLine($"genericBookDictionary -- Entry '11' book ISBN: '{genericBookDictionary["11"].Isbn}'");
+			Console.WriteLine($"genericBookDictionary -- Entry '11' book title: '{genericBookDictionary["11"].Title}'");
 		}
 
+		//Book
 		public class Book
 		{
 			private string _isbn;
@@ -42,33 +64,37 @@ namespace Mosh
 			public string Isbn { get { return _isbn; } }
 		}
 
-		public class GenericList<T> //'T' stands for type
+		//List
+		public class GenericClassList<T> //'T' stands for type
 		{
 			private T _genericValue;
-			public void Add(T genericValue)
-			{ _genericValue = genericValue; }
+			
+			//Add to list
+			public void AddToList(T genericValue) { _genericValue = genericValue; }
 
-			//Indexer - Get value of 'this' generic list at 'index'
-			public T this[int index]
-			{ get { return _genericValue; } }
+			//Indexer - Get value of 'this' generic list at 'index' value, and return 'T'
+			public T this[int index] { get { return _genericValue; } }
 		}
 
-		public class GenericsDictionary<TKey, TValue>
+		//Dictionary
+		public class GenericDictionary<TKey, TValue>
 		{
+			//Dictionaries take two inputs:
+			// - Key: the position name within the dictionary
+			// - Value: The value of that position
 			private Dictionary<TKey, TValue> _dictionary = new Dictionary<TKey, TValue>();
 
-			public void AddToDictionary(TKey key, TValue value)
-			{
-				_dictionary.Add(key, value);
-			}
+			//Add to dictionary
+			public void AddToDictionary(TKey key, TValue value) { _dictionary.Add(key, value); }
 
-			//Return 'TValue' by 'TKey'
-			public TValue this[TKey key]
-			{ get { return _dictionary[key]; } }
+			//Indexer - Get value of 'this' dictionary at 'TKey' value, and return 'TValue'
+			public TValue this[TKey key] { get { return _dictionary[key]; } }
 		}
 
-												//Adds IComparable 'constraint' to T
-		public class GenericsNumberProcessing<T> where T : IComparable
+		//////////////////////////////////////////////////////////////////
+
+												
+		public class GenericNumberProcessing<T> where T : IComparable	//Adds IComparable 'constraint' to T
 		{
 			public T ReturnMaxNumber(T num1, T num2)
 			{
