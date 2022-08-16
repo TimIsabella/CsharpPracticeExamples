6cb0874180899b6ed227bdc969dd71bf60608687
TimIsabella <timjisabella@gmail.com>
2022-08-16 14:36:41 -0700

    Enums

---
+++ Mosh/EnumExamples.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mosh
+{
+	public class EnumExamples
+	{
+		public static void EnumExamplesMain()
+		{
+			var enumNumber = RandomEnums.Number3;
+			Console.WriteLine("The enum is enumNumber");
+		}
+
+		//enum - short for enumeration
+		//an array of constants accessed by dot notation
+		//Can only be integers
+		public enum RandomEnums
+		{
+			Number1 = 11,
+			Number2 = 22,
+			Number3 = 33,
+			Number4 = 44,
+			Number5 = 55,
+			Number6 = 66
+		}
+	}
+}
