7e8986dffe6db06b4c99c83184772d3494b4b1ec
TimIsabella <timjisabella@gmail.com>
2022-08-15 17:25:58 -0700

    Generics

--- Mosh/GenericsExamples.cs
+++ Mosh/GenericsExamples.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Mosh
 {
@@ -10,7 +7,52 @@ namespace Mosh
 	{
 		public void GenericsExamplesMain()
 		{
-			
+			var book = new Book(new string[] { "1111", "Things about numbers by Nmbrfg" });
+
+			var numbersList = new List<int>();
+			numbersList.Add(11);
+
+			var booksList = new BookList();
+			booksList.Add(book);
+
+			var numbersGenericsList = new GenericList<int>();
+			numbersGenericsList.Add(22);
+
+			var booksGenericsList = new GenericList<Book>();
+			booksGenericsList.Add(new Book(new string[] { "1234", "Generic book" } ));
+		}
+
+		public class Book
+		{
+			private string _isbn;
+			private string _title;
+			public Book(string[] bookArray)
+			{
+				_isbn = bookArray[0];
+				_title = bookArray[1];
+			}
+		}
+
+		//Older non-generics way
+		public class BookList
+		{
+			public void Add(Book book)
+			{ throw new NotImplementedException(); }
+
+			//Get book at index
+			public Book this[int index]
+			{ get { throw new NotImplementedException(); } }
+		}
+
+		//Newer 'Generics' way 
+		public class GenericList<T> //'T' stands for type
+		{
+			public void Add(T genericValue)
+			{ }
+
+			//Get 'generic' at index
+			public T this[int index]
+			{ get { throw new NotImplementedException(); } }
 		}
 	}
 }
