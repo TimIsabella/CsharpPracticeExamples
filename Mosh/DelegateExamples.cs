39c0af7375abe4f9a61b431b1f3bcc44e0d2f63b
TimIsabella <timjisabella@gmail.com>
2022-08-16 19:12:15 -0700

    Delegate

--- Mosh/DelegateExamples.cs
+++ Mosh/DelegateExamples.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Mosh
 {
@@ -11,6 +7,22 @@ namespace Mosh
 		public static void DelegateExamplesMain()
 		{
 			Console.WriteLine("\n *********** DELEGATE *********** \n");
+
+			var processor = new PhotoProcessor();
+
+			/////////// Delegate ///////////
+
+			PhotoProcessor.PhotoFilterHandler filterHandler; //Instantiate the delegate container
+			filterHandler = RemoveRedEyeFilter;				 //Add mathod of matching signature to delegate container
+			filterHandler += PhotoSize.Resize;               //Add mathod of matching signature to delegate container
+
+			var filters = new PhotoFilters(); 
+			filterHandler += filters.ApplyBrightness;        //Add mathod of matching signature to delegate container
+			filterHandler += filters.ApplyContrast;          //Add mathod of matching signature to delegate container
+
+			/////////////////////////////////
+
+			processor.Process("photo.jpg", filterHandler);	 //All delegated methods under 'filter handler' will be called
 		}
 
 		public class Photo
@@ -26,29 +38,45 @@ namespace Mosh
 
 		public class PhotoProcessor
 		{
-			public void Process(string path)
+			//'delegate' -- holds a pointer to a method or a group of methods
+			//Inherits the resulting methods for this class to utilize
+			//Establishes the delegate signature as 'methodName(Photo photo)'
+			public delegate void PhotoFilterHandler(Photo photo);
+
+			public void Process(string path, PhotoFilterHandler filterHandler)
 			{
 				var photo = Photo.Load(path);
 
-				var filters = new PhotoFilters();
-				filters.ApplyBrightness(photo);
-				filters.ApplyContrast(photo);
-				filters.Resize(photo);
-				
+				//Delegate
+				filterHandler(photo);
+
 				photo.Save();
 			}
 		}
 
+		/////////// Filters ///////////
+
+		//Matches delegate signature
+		public static void RemoveRedEyeFilter(Photo photo)
+		{ Console.WriteLine("Apply red-eye filter"); }
+
 		public class PhotoFilters
 		{
+			//Matches delegate signature
 			public void ApplyBrightness(Photo photo)
 			{ Console.WriteLine("Apply brightness"); }
 
+			//Matches delegate signature
 			public void ApplyContrast(Photo photo)
 			{ Console.WriteLine("Apply contrast"); }
+		}
 
-			public void Resize(Photo photo)
+		public class PhotoSize
+		{
+			//Matches delegate signature
+			public static void Resize(Photo photo)
 			{ Console.WriteLine("Resize photo"); }
 		}
+		
 	}
 }
--- Mosh/Program.cs
+++ Mosh/Program.cs
@@ -81,6 +81,7 @@ namespace Mosh
 			InterfaceExtensibilityExamples.InterfaceExtensibilityExamplesMain();
 			InterfacePolymorphismExamples.InterfacePolymorphismExamplesMain();
 			GenericsExamples.GenericsExamplesMain();
+			DelegateExamples.DelegateExamplesMain();
 		}
 	}
 }
