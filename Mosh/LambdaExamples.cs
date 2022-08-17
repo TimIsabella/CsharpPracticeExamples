3d3382027fd83146c4abb5eee1e89712579d9e7b
TimIsabella <timjisabella@gmail.com>
2022-08-16 19:38:41 -0700

    Lambda

---
+++ Mosh/LambdaExamples.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Mosh
+{
+	public class LambdaExamples
+	{
+		public void LambdaExamplesMain()
+		{
+			Console.WriteLine("\n *********** LAMBDA EXAMPLES *********** \n");
+
+			Console.WriteLine(Multiply(123, 456, 789));
+
+			//Same as above using lambda
+			//'Func' delagate
+			Func<int, int, int, int> multiplyByDelegate = (num1, num2, num3) => num1 * num2 * num3;
+			Console.WriteLine(multiplyByDelegate(123, 456, 789));
+		}
+
+		public static int Multiply(int num1, int num2, int num3)
+		{
+			return num1 * num2 * num3;
+		}
+	}
+}
