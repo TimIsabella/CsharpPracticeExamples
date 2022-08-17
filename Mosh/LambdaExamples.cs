1b9ec255972ce8472a8f888a646f46e448366db8
TimIsabella <timjisabella@gmail.com>
2022-08-16 19:44:53 -0700

    Lambda

--- Mosh/LambdaExamples.cs
+++ Mosh/LambdaExamples.cs
@@ -12,8 +12,12 @@ namespace Mosh
 
 			//Same as above using lambda
 			//'Func' delagate
-			Func<int, int, int, int> multiplyByDelegate = (num1, num2, num3) => num1 * num2 * num3;
-			Console.WriteLine(multiplyByDelegate(123, 456, 789));
+			Func<int, int, int, int> multiplyByDelegate1 = (num1, num2, num3) => num1 * num2 * num3;
+			Console.WriteLine(multiplyByDelegate1(123, 456, 789));
+
+			const int number2 = 456;
+			Func<int, int, int> multiplyByDelegate2 = (num1, num3) => num1 * number2 * num3;
+			Console.WriteLine(multiplyByDelegate2(123, 789));
 		}
 
 		public static int Multiply(int num1, int num2, int num3)
