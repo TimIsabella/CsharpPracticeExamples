b2882260211436c688a011110ea3dfb1d0f722c7
TimIsabella <timjisabella@gmail.com>
2022-08-23 09:37:09 -0700

    Changes

---
+++ Mosh/Fundimentals/UpDownCastingPractice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Mosh
+{
+	public class UpDownCastingPractice
+	{
+		public static void UpDownCastingPracticeMain()
+		{
+			Console.WriteLine("\n *********** UP/DOWN CASTING PRACTICE *********** \n");
+
+			
+		}
+
+
+	}
+}
