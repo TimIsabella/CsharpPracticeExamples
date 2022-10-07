f6a741d6ccca12fb195431c6a2f4591d6027fe11
TimIsabella <timjisabella@gmail.com>
2022-10-07 13:46:08 -0700

    Generics

--- PracticeExamples/Fundamentals/GenericsExamplesPractice2.cs
+++ PracticeExamples/Fundamentals/GenericsExamplesPractice2.cs
@@ -9,53 +9,37 @@ namespace PracticeExamples
 		{
 			Console.WriteLine("\n *********** GENERICS EXAMPLES PRACTICE 2 *********** \n");
 
-			int[] intArray = { 1, 2, 3 };
-			double[] doubleArray = { 1.1, 2.2, 3.3 };
-			string[] stringArray = { "one", "two", "three" };
-
-			DisplayElements(intArray, doubleArray, stringArray);
-
-			DisplayElementsGeneric(intArray);
-			DisplayElementsGeneric(doubleArray);
-			DisplayElementsGeneric(stringArray);
-
-			//Cannot pass in multiple types as generic, only one type at a time
-			DisplayElementsGenericMultiple(intArray, null, null);
-			DisplayElementsGenericMultiple(null, doubleArray, null);
-			DisplayElementsGenericMultiple(null, null, stringArray);
+			var genericClass = new GenericsClass();
+			genericClass.OutputGeneric("string");
+			genericClass.OutputGenericArray(new int[] { 1, 2, 3 });
+			genericClass.OutputGenericMultiple(1.618, new double[] { 1.23, 4.56, 7.89 });
 		}
 
-		public static void DisplayElements(int[] intA, double[] dubA, string[] strA)
-		{
-			foreach(int i in intA)
-			{ Console.WriteLine(i); }
-		}
+		//////////////////////////////////////////////////////////////////
 
-		public static void DisplayElementsGeneric<T>(T[] thing)
+		class GenericsClass
 		{
-			foreach(T i in thing)
-			{ Console.WriteLine(i); }
-		}
+			//'T' is the name for a generic type to be used
+			public void OutputGeneric<T>(T genericT)
+			{ Console.WriteLine($"OutputGeneric T genericT: {genericT}"); }
 
-		public static void DisplayElementsGenericMultiple<T>(T[] intA, T[] dubA, T[] strA)
-		{
-			if(intA != null)
+			//The generic type can be named anything
+			public void OutputGenericArray<Z>(Z[] genericZarray)
 			{ 
-				foreach(T i in intA)
-				{ Console.WriteLine($"intA: {i}"); }
+				foreach(Z i in genericZarray)
+				{ Console.WriteLine($"OutputGenericArray T[] genericT: {i}"); }
 			}
 
-			if(dubA != null)
+			//The type is cast as only ONE type
+			//- In the below case, both parameters must be the same type
+			public void OutputGenericMultiple<Thing>(Thing genericThing, Thing[] genericThingArray)
 			{
-				foreach(T i in dubA)
-				{ Console.WriteLine($"dubA: {i}"); }
-			}
+				Console.WriteLine($"OutputGenericMultiple Thing genericThing: {genericThing}");
 
-			if(strA != null)
-			{
-				foreach(T i in strA)
-				{ Console.WriteLine($"strA: {i}"); }
+				foreach(Thing i in genericThingArray)
+				{ Console.WriteLine($"OutputGenericMultiple Thing[] genericThingArray: {i}"); }
 			}
 		}
+		
 	}
 }
