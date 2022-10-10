dbe4241c343b217d3b2e5e0bed2f9ab88af4e0e3
TimIsabella <timjisabella@gmail.com>
2022-10-09 23:09:18 -0700

    ,,,

--- CsharpPracticeExamples/DotNet/Fundamentals/IQueryableExamples.cs
+++ CsharpPracticeExamples/DotNet/Fundamentals/IQueryableExamples.cs
@@ -8,7 +8,14 @@ namespace PracticeExamples.DotNet.Fundamentals
 	{
 		public static void IQueryableExamplesMain()
 		{
-			Console.WriteLine("\n *********** IQUERYABLE EXAMPLES *********** \n");    
+			Console.WriteLine("\n *********** IQUERYABLE EXAMPLES *********** \n");
+
+			var personYeild = new Person { Id = 3, Name = "Bill Williams", Location = "Texas" };
+
+			foreach(var name in personYeild.Names)
+			{ Console.WriteLine(name); }
+
+			/// //////////////////////////////////////////////////////////////////
 
 			var peopleList = new List<Person>() { 
 													new Person { Id = 0, Name = "John Doe", Location = "California"},
@@ -21,8 +28,6 @@ namespace PracticeExamples.DotNet.Fundamentals
 											new Person { Id = 1, Name = "Sarah Connor", Location = "Nevada"},
 											new Person { Id = 2, Name = "Jake Phillips", Location = "Florida"},
 										 };
-
-			//////////////////////////////////////////////////////////////////
 			
 			//Array converted and extended by 'IEnumerable'
 			//- 'IEnumerable' extension methods can now be called on the array
@@ -39,39 +44,55 @@ namespace PracticeExamples.DotNet.Fundamentals
 			//'IEnumerable' collection cast as 'IQueryable'
 			IQueryable<Person> peopleEnumerableQueryable = (IQueryable<Person>)peopleArrayEnumerable; //'Explicit conversion' using parenthesis
 
-			//////////////////////////////////////////////////////////////////
+			/// //////////////////////////////////////////////////////////////////
 
 			//Returns an 'IQueryable'
 			//- Whole object is returned
-			var output1 = peopleListQueryable.Where(person => person.Id == 1);
+			var queryableQuery1 = peopleListQueryable.Where(person => person.Id == 1);
 
 			///Below doesn't work because it is an 'IQueryable' object
-			//Console.WriteLine($" Person ID {output1.Id} = {output1.Name} is in {output1.Location}");
+			//Console.WriteLine($" Person ID {queryableQuery1.Id} = {queryableQuery1.Name} is in {queryableQuery1.Location}");
 
 			//'foreach' is part of 'IQueryable'
-			foreach(Person person in output1)
+			foreach(Person person in queryableQuery1)
 			{ Console.WriteLine($" Person ID {person.Id} = '{person.Name}' who lives in '{person.Location}'"); }
 
 			//Returns an 'IQueryable'
 			//- Object string 'Name' is returned
-			var output2 = from people
-						  in peopleEnumerableQueryable
-						  where people.Id == 2
-						  select people.Name;
+			var queryableQuery2 = from people
+								  in peopleEnumerableQueryable
+								  where people.Id == 2
+								  select people.Name;
 
 			///Below doesn't work because it is an 'IQueryable' string
-			//Console.WriteLine($" Person ID 2 Name = '{output2.Name}'");
+			//Console.WriteLine($" Person ID 2 Name = '{queryableQuery2.Name}'");
 
 			//'foreach' is part of 'IQueryable'
-			foreach(string person in output2)
+			foreach(string person in queryableQuery2)
 			{ Console.WriteLine($" Person ID 2 Name = '{person}'"); }
 		}
+
+		///////////////////////////////////////////////////////////////////////////////////////////////////
 
 		public class Person
 		{
 			public int Id { get; set; }
 			public string Name { get; set; }
 			public string Location { get; set; }
+
+			//'IEnumerable' property with mixed ints and strings
+			public IEnumerable<object> Names
+			{
+				get
+				{
+					yield return Id;
+					yield return Name;
+					yield return Location;
+					yield return "Nickname";
+					yield return "Alias";
+					yield return 123456;
+				}
+			}
 		}
 	}
 }
