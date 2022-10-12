88e646a596fdcf952023cd3da138cee6aede46f9
TimIsabella <timjisabella@gmail.com>
2022-10-12 14:18:31 -0700

    Task

--- CsharpPracticeExamples/DotNet/Fundamentals/TaskExamples.cs
+++ CsharpPracticeExamples/DotNet/Fundamentals/TaskExamples.cs
@@ -15,29 +15,37 @@ namespace PracticeExamples.DotNet.Fundamentals
 			/// A task is a unit of work
 			///'Task' is a .Net class which provides for asynchronus operations
 
-			///Below tasks all run asynchronusly
+			/// Below tasks all run asynchronusly
 			///- They compete for the same console space which causes output overlapping issues
-			var taskLogic = new TaskLogic();
 
-			// Create and start a 'Task'
-			Task.Factory.StartNew(() => taskLogic.WriteChars('.'));
+			/// Create and start a 'Task' ///
+			var taskLogic1 = new TaskLogic();
+			Task.Factory.StartNew(() => taskLogic1.WriteChars('.'));
 
 			//Readline can be used as a break to prevent the asynchronus overlap
 			//Console.ReadLine();
 
-			// Create new 'Task' and then start the task
+			/// Create new 'Task' and then start the task ///
 			//- Logic passed in by lambda
-			var task1 = new Task(() => taskLogic.WriteChars('?'));
+			var taskLogic2 = new TaskLogic();
+			var task1 = new Task(() => taskLogic2.WriteChars('?'));
 			task1.Start();
 
 			//Console.ReadLine();
 
-			// Create new 'Task' and then start the task
+			/// Create new 'Task' and then start the task ///
 			//- Logic passed in as 'action' and 'state' by 'object'
-			var task2 = new Task(taskLogic.WriteObjects, "Object argument");
+			var taskLogic3 = new TaskLogic();
+			var task2 = new Task(taskLogic3.WriteObjects, "Object argument");
 			task2.Start();
 
 			//Console.ReadLine();
+
+			string text1 = "testing";
+			string text2 = "this";
+
+			var task3 = new Task<int>(taskLogic3.TextLength, text1);
+			task3.Start();
 		}
 
 		///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -66,6 +74,7 @@ namespace PracticeExamples.DotNet.Fundamentals
 
 			public int TextLength(object objectParam)
 			{
+				Console.WriteLine($"Current Task Id {Task.CurrentId} is currently processing object '{objectParam}'");
 				return objectParam.ToString().Length;
 			}
 		}
