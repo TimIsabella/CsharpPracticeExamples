bc892a8583dd64c0b7e270aebbb6fc2a601ac8e2
TimIsabella <timjisabella@gmail.com>
2022-10-13 11:47:41 -0700

    Task

--- CsharpPracticeExamples/DotNet/Fundamentals/TaskExamples.cs
+++ CsharpPracticeExamples/DotNet/Fundamentals/TaskExamples.cs
@@ -34,18 +34,33 @@ namespace PracticeExamples.DotNet.Fundamentals
 			//Console.ReadLine();
 
 			/// Create new 'Task' and then start the task ///
-			//- Logic passed in as 'action' and 'state' by 'object'
 			var taskLogic3 = new TaskLogic();
+
+			//Instantiate new 'Task' while passing in action and state
+			//'action' is the 'taskLogic3' method and 'state' is the string
+			//'action' has no return type
 			var task2 = new Task(taskLogic3.WriteObjects, "Object argument");
 			task2.Start();
 
-			//Console.ReadLine();
+			Console.ReadLine();
 
+			/// Create new generic 'Task' ///
+			var taskLogic4 = new TaskLogic();
 			string text1 = "testing";
 			string text2 = "this";
 
-			var task3 = new Task<int>(taskLogic3.TextLength, text1);
+			//Instantiate new 'Task' while passing in action and state
+			//'action' is the 'taskLogic4' method and 'state' is the string
+			//'action' method return type is 'int' so 'Task' must provide generic for 'int'
+			var task3 = new Task<int>(taskLogic4.TextLength, text1);
 			task3.Start();
+
+			Console.WriteLine($"Task 3 output length: '{task3.Result}'");
+
+			//Same as above as above but automatically starts with '.StartNew'
+			Task<int> task4 = Task.Factory.StartNew<int>(taskLogic4.TextLength, text2);
+
+			Console.WriteLine($"Task 4 output length: '{task4.Result}'");
 		}
 
 		///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -74,7 +89,7 @@ namespace PracticeExamples.DotNet.Fundamentals
 
 			public int TextLength(object objectParam)
 			{
-				Console.WriteLine($"Current Task Id {Task.CurrentId} is currently processing object '{objectParam}'");
+				Console.WriteLine($"\nCurrent Task Id {Task.CurrentId} is currently processing object '{objectParam}'...");
 				return objectParam.ToString().Length;
 			}
 		}
